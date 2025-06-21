17ccb6ecd77e819be9a40491a9fff4384b0b2cda
pallavilagisetti <pallavilagisetti2003@gmail.com>
2025-06-21 11:43:13 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<your-repo-name>/',
+
   plugins: [react(),tailwindcss()],
+  base:process.env.VITE_BASE_PATH || "/assignment",
 })
